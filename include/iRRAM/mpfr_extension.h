77c380c9d57553e114b253630dcb1283c7096730
Franz Brauße <s4frbrau@uni-trier.de>
2014-10-22 22:05:50 +0200

    Delete duplicate (and incompatible) declaration of iRRAM_initialize().

--- include/iRRAM/mpfr_extension.h
+++ include/iRRAM/mpfr_extension.h
@@ -17,8 +17,6 @@ void mpfr_ext_cos (mpfr_ptr r, mpfr_srcptr u,int p);
 void mpfr_ext_tan (mpfr_ptr r, mpfr_srcptr u,int p);
 void mpfr_ext_test (mpfr_ptr r, mpfr_srcptr u,int p,mp_rnd_t rnd_mode);
 
-void iRRAM_initialize(int argc,char **argv);
-
 #ifdef __cplusplus
 }
 #endif
